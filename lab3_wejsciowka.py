c21bacb8766fb020561f0addf6da4ef607b34373
w41332 <w41332@student.wsiz.edu.pl>
2023-11-13 12:06:09 +0100

    p2

---
+++ lab3_wejsciowka.py
@@ -0,0 +1,44 @@
+
+if __name__ == '__main__':
+  print("1. liczby parzyste")
+  print("2. liczba dodatnia")
+  zadanie = int(input("podaj numer zadania: "))
+  if zadanie == 1 :
+
+    a = int(input("podaj pierwszą liczbe "))
+    b = int(input("podaj drugą liczbe "))
+
+    if a > b:
+      k = a
+      p = b
+    elif b > a:
+      k = b
+      p = a
+    else:
+        print("nie licze bo liczby są rowne")
+        k = a
+        p = b
+        p-=1
+
+    if a != b:
+        while (p <= k):
+            p += 1
+            if p % 2:
+                continue
+            print(p)
+
+        else:
+            print("end")
+  elif zadanie == 2 :
+      liczba=0
+      while(True):
+          liczba=int(input("podaj liczbe dodatnia: "))
+
+          if liczba>0:
+              break
+          else:
+            print("Podałes złą liczbe spróbuj ponownie.")
+
+      print(f"twoja dodatnia liczba to{liczba}")
+
+
