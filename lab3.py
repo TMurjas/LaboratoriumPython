0457dc51cbd7c7e02d9faba776641c5bc06ffdab
w41332 <w41332@student.wsiz.edu.pl>
2023-11-13 12:49:37 +0100

    p2

---
+++ lab3.py
@@ -0,0 +1,54 @@
+if __name__ == '__main__':
+    print("1. petla for przedziały liczb")
+    print("2. wyswietl *** gwiazdki")
+    zadanie=int(input("podaj nr zadania: "))
+    if zadanie == 1:
+        print("wypisz od 1 do 100")
+        for x in range (100):
+            print(x+1)
+
+        print("wypisz od 100 do 0")
+        for x in range(100, -1, -1):
+            print(x)
+
+        print("wypisz od 7 do 77 co 7")
+        for x in range(7, 78, 7):
+            print(x)
+
+        print("wypisz od 20 do 0 co 2")
+        for x in range(0, 22, 2):
+            print(20-x)
+########################################
+    elif zadanie == 2:
+        gwiazdki = int(input("podaj ile razy ma wyswietlic ***: "))
+        for x in range(gwiazdki):
+            for y in range(gwiazdki):
+              print("* ", end="")
+        print()
+########################################
+    elif zadanie == 3:
+        gwiazdki = int(input("podaj ile razy ma wyswietlic * : "))
+        star = ""
+        for x in range(gwiazdki):
+          star += "*"
+          print(star)
+########################################
+    elif zadanie == 4:
+        gwiazdki = int(input("podaj ile razy ma wyswietlic ***: "))
+        star = ""
+        space = " "
+        for x in range(gwiazdki):
+            for y in range(gwiazdki-x):
+                space+=" "
+            star += "*"
+            print(space+star)
+            space = " "
+
+     elif zadanie == 5:
+         n = int(input("długosć ciągu N: "))
+         a = int(input("pierwszy wyraz ciagu: "))
+         r = int(input("roznica ciągu: "))
+
+         for i in range(n):
+
+
