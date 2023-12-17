e7eef18cb601ccdd1e391e95a289bfa86af78afb
TMurjas <w41332@student.wsiz.edu.pl>
2023-12-17 20:29:14 +0100

    pr2

--- lab4.py
+++ lab4.py
@@ -7,9 +7,14 @@ if __name__ == '__main__':
     zad=1
     if zad == 1:
       lista=["Tomek","Romek","Arek","Mirek"]
-      print(sorted(lista))
-
-
+      lista=sorted(lista)
+      print(lista)
+      lista.append("Antek")
+      lista.append("Ola")
+      zmienna=lista.pop()
+      print(zmienna)
+      lista.insert(3, "aga")
+      print(lista)
 
     if zad==2:
         pass
