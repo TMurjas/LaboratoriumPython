7f3a8ee15ff474a7dd9db603ed4359f46fcf2a34
TMurjas <w41332@student.wsiz.edu.pl>
2024-01-21 18:56:16 +0100

    pr2

--- lab6.py
+++ lab6.py
@@ -1,8 +1,12 @@
 import random
 import math
 import time
+import keyword
+import datetime
+import statistics
+import calendar
 
-zad=4
+zad=10
 if zad==1:
    print(f"szczesliwy numerek to : {random.randint(0, 99)}")
    iloscWGrupie=input("podaj ile osub jest w grupie: ")
@@ -56,4 +60,92 @@ elif zad == 3:
     print("czas upłynoł")
 
 elif zad == 4:
+    def numOfDays(date1, date2):
+        # check which date is greater to avoid days output in -ve number
+            return (date1 - date2).days
+
+
+
+    date1 = datetime.date(2023, 12, 19)
+    date2 = datetime.date(2024, 1, 15)
+    date3 = datetime.date(2024, 2, 12)
+    print(numOfDays(date2, date1), "dni")
+    print(numOfDays(date3, datetime.date.today()), "dni do kolokwium")
+
+elif zad == 5:
+    klucze=["for","print","break","done","bad"]
+    for i in range(len(klucze)):
+      if keyword.iskeyword(klucze[i]):
+         print(klucze[i]," jest słowek kluczowym")
+      else:
+          print(klucze[i], " nie jest słowek kluczowym")
+
+elif zad == 6:
+    print(dir(math))
+    print(dir(tuple))
+    print(dir(keyword))
+elif zad == 7:
+    ran1 = int(input("podaj dolny przedział: "))
+    ran2 = int(input("podaj górny przedział: "))
+    tab=[""]*10
+    for i in range(len(tab)):
+        tab[i]=random.randint(ran1, ran2)
+    krotka= tuple(tab)
+    print(krotka)
+    print(statistics.geometric_mean(krotka))
+elif zad == 8:
+        a, b, c = input("Podaj 3 boki trójkąta (oddzielone przecinkami): ")
+
+        if a + b > c and a + c > b and b + c > a:
+            print("Z podanych boków można zbudować trójkąt.")
+            if (a ** 2 + b ** 2 > c ** 2 or
+                    a ** 2 + c ** 2 > b ** 2 or
+                    b ** 2 + c ** 2 > a ** 2):
+
+                print ("Obwód wynosi:", (a + b + c))
+                pole = 0.5 * (a + b + c)
+                pole = math.sqrt(pole * (pole - a) * (pole - b) * (pole - c))
+                print ("Pole wynosi:", pole)
+        else:
+            print
+            "Z podanych odcinków nie można utworzyć trójkąta"
+elif zad == 9:
+    ran1 = int(input("podaj dolny przedział: "))
+    ran2 = int(input("podaj górny przedział: "))
+    liczba=random.randint(ran1, ran2)
+
+    i=3
+    while(i>0):
+        print(f"masz {i} szanse")
+        odp = int(input("podaj wylosowaną liczbe: "))
+        if odp==liczba:
+            print("Wygrałes wylosowana liczba to: ",liczba)
+            break;
+        elif odp>liczba:
+            print("twoja liczba jest za duża")
+        elif odp < liczba:
+            print("twoja liczba jest za mała")
+        i-=1
+    else:
+        print(f"Nie udało sie liczba wynosiła {liczba} spróbuj ponownie")
+
+elif zad == 10:
+    rok = int(input("podaj rok: "))
+    mie = int(input("podaj miesiąc: "))
+    dni = int(input("podaj dzień: "))
+    date1 = datetime.date(rok, mie, dni)
+    print(date1)
+    print("nr tygodnia to: ",date1.strftime("%U"))
+
+    for i in range(6):
+        date2 = date1 + datetime.timedelta(days=i)
+        if date2.strftime("%A")=="Sunday":
+            break
+    print(date2,date2.strftime("%A"))
+    print(date1 + datetime.timedelta(days=14))
+    print(date1 - datetime.timedelta(days=14))
+    date = datetime.datetime.utcnow()
+    utc_time = calendar.timegm(date.utctimetuple())
+    print(utc_time)
+
 
